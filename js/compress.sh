59826f2649276893e2b4c18b9394d6dc6b883ba0
ilmikko <mikko@5x.fi>
2018-05-05 14:30:30 +0100

    Added js compressor script using UglifyJS

---
+++ js/compress.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# This simple bash script combines the js files into one app.min.js
+
+uglifyjs --compress --mangle -- alt.js loading-handler.js vertical-slider.js player.js > app.min.js
