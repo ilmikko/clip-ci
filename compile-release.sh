94e0d30290ac67619f7c43cbc13285148e53ff6e
ilmikko <mikko@5x.fi>
2018-05-12 15:04:40 +0100

    HTML is now separately handled

--- compile-release.sh
+++ compile-release.sh
@@ -17,25 +17,25 @@ printf "player.load(" > ./player.json;
 cat demo/demo.json | tr -d ' \t\n\r\f' >> ./player.json;
 printf ");\n" >> ./player.json;
 
-echo > index.min.html;
+echo > index.html;
 
 # Include the json
-sed -e '/player.load({})/rplayer.json' index.html | sed -e 's/\/\/.*$//' > index.min.html;
+sed -e '/player.load({})/rplayer.json' html/index.template.html | sed -e 's/\/\/.*$//' > index.html;
 
 # Remove other scripts
-cat index.min.html | sed -e 's/<script .*><\/script>//' | tee index.min.html;
+cat index.html | sed -e 's/<script .*><\/script>//' | tee index.html;
 
 # Add the ./js/app.min.js we just compiled
 echo "<script src='./js/app.min.js'></script>" > append.html;
 
 echo "basdbasbd";
-html=$(sed -e '/<!-- App scripts -->/rappend.html' index.min.html);
-echo $html > index.min.html;
+html=$(sed -e '/<!-- App scripts -->/rappend.html' index.html);
+echo $html > index.html;
 
 # Remove comments and whitespace
 # TODO: Remove comments
-html=$(cat index.min.html | tr -d '\t\n\r\f');
-echo $html > index.min.html;
+html=$(cat index.html | tr -d '\t\n\r\f');
+echo $html > index.html;
 
 # remove the temp files
-rm index.min.html append.html player.json;
+rm append.html player.json;
