a889dba5357b21f7f336829c72c60d824e3a3311
ilmikko <mikko@5x.fi>
2018-05-12 15:07:46 +0100

    Do not destroy whitespace when compiling

--- compile-release.sh
+++ compile-release.sh
@@ -14,13 +14,11 @@ cd ..
 echo "Compiling HTML...";
 
 printf "player.load(" > ./player.json;
-cat demo/demo.json | tr -d ' \t\n\r\f' >> ./player.json;
+cat demo/demo.json >> ./player.json;
 printf ");\n" >> ./player.json;
 
-echo > index.html;
-
 # Include the json
-sed -e '/player.load({})/rplayer.json' html/index.template.html | sed -e 's/\/\/.*$//' > index.html;
+sed -e '/player.load({})/rplayer.json' html/index.template.html > index.html;
 
 # Remove other scripts
 cat index.html | sed -e 's/<script .*><\/script>//' | tee index.html;
@@ -28,14 +26,8 @@ cat index.html | sed -e 's/<script .*><\/script>//' | tee index.html;
 # Add the ./js/app.min.js we just compiled
 echo "<script src='./js/app.min.js'></script>" > append.html;
 
-echo "basdbasbd";
-html=$(sed -e '/<!-- App scripts -->/rappend.html' index.html);
-echo $html > index.html;
-
-# Remove comments and whitespace
-# TODO: Remove comments
-html=$(cat index.html | tr -d '\t\n\r\f');
-echo $html > index.html;
+html="$(sed -e '/<!-- App scripts -->/rappend.html' index.html)";
+echo "$html" > index.html;
 
 # remove the temp files
 rm append.html player.json;
